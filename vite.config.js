c2262ed76f16e7e08d188e1319dec8c138969738
Kasfex <kasfex@gmail.com>
2025-05-13 00:05:44 +0100

    deploy configured

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  base: "/ebenai-site/",
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["lucide-react"],
+  },
+  server: {
+    host: true, // Allows external access
+    allowedHosts: [".ngrok-free.app"], // Allow ngrok
+  },
 });
